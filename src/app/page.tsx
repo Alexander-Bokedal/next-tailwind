5d0344091c5047a350582a2c4ffb00d05895c0f1
Alexander-Bokedal <alexander.bokedal@gmail.com>
2025-03-22 11:56:16 +0100

    lasagna
    garfield

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ExpandableButtons from "@/components/expandableButtons";
 
 export default function Home() {
   return (
-    <div className='flex flex-col text-black bg-teal-950 h-dvh items-center justify-center gap-y-4 p-4'>
+    <div className='grid flex-col text-black bg-teal-950 h-dvh items-center justify-center gap-y-4 p-4'>
       <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-8">
         <ExpandableButtons />
         <ExpandableAccordians />
